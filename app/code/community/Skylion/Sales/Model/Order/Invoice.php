a4ef1e5c1bdb8ef70801c72f3a3903ce54392995
kayheunen <kay.heunen@gmail.com>
2015-02-12 13:09:23 +0100

    little fix for magento 1.7
    thanks @vinary28

--- app/code/community/Skylion/Sales/Model/Order/Invoice.php
+++ app/code/community/Skylion/Sales/Model/Order/Invoice.php
@@ -4,7 +4,7 @@
 	class Skylion_Sales_Model_Order_Invoice extends Mage_Sales_Model_Order_Invoice {
 		public function getCreatedAtFormated($format)
 	    {
-	        return Mage::helper('core')->formatDate($this->getcreated_at(), $format);
+	        return Mage::helper('core')->formatDate($this->getCreatedAt(), $format);
 	    }
 	}
 ?>
